9566069a17c985855b21e7f0792772ed96356675
Fan Hu <fanhu@au1.ibm.com>
2024-07-04 19:29:04 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createApp } from '@backstage/core-app-api';
+import { Router } from '@backstage/plugin-catalog';
+import { EntityPage, EntityLayout, EntityLayoutRoute } from '@backstage/plugin-catalog';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = createApp({
+  components: {
+    // other components
+  },
+  bindRoutes: ({ bind }) => {
+    bind(catalogPlugin.externalRoutes, {
+      createComponent: createComponentRouteRef,
+    });
+  },
+});
+
+const CustomEntityPage = () => (
+  <EntityLayout>
+    <EntityLayout.Route path="/" title="History">
+      <OverviewComponent />
+    </EntityLayout.Route>
+  </EntityLayout>
+);
+
+const App = () => (
+  <AppProvider app={app}>
+    <Router>
+      <CustomEntityPage />
+    </Router>
+  </AppProvider>
+);
 
 export default App;
