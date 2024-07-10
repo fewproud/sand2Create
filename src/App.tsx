68487dedf38bae958803e1596a16d54239fd69b1
Fan Hu <fanhu@au1.ibm.com>
2024-07-10 13:18:50 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { createApp } from '@backstage/app-defaults';
 import { JenkinsPage } from '../dynamic-plugins/dist/backstage-plugin-jenkins'; // Adjust this path as necessary
 import { AppBar, Tabs, Tab, Toolbar } from '@material-ui/core'; // Import necessary UI components
 
-
 // Initialize the Backstage app
 const app = createApp({
   // You can customize your app's components and settings here
@@ -19,11 +18,13 @@ const App = () => (
       <Toolbar>
         <Tabs>
           <Tab label="Jenkins" component="a" href="/jenkins" />
+          {/* Add more tabs as needed */}
         </Tabs>
       </Toolbar>
-    </AppBar>    
+    </AppBar>
     <Routes>
       <Route path="/jenkins" element={<JenkinsPage />} />
+      {/* Define other routes here */}
     </Routes>
   </BrowserRouter>
 );
